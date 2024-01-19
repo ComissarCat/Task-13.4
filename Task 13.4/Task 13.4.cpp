44eb8d8b2d266a880147525f5b6bdb75c30c704f
Медведев Андрей Юрьевич <andrey.medvede@yandex.ru>
2024-01-19 10:53:11 +0300

    Добавьте файлы проекта.

---
+++ Task 13.4/Task 13.4.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Multiplication_table
+{
+private:
+    vector <vector <int>> table;
+public:
+    Multiplication_table()
+    {
+        vector<int> line;
+        for (int i = 1; i <= 10; i++)
+        {
+            line.clear();
+            for (int j = 1; j <= 10; j++)
+            {
+                line.push_back(i * j);
+            }
+            table.push_back(line);
+        }
+    }
+    friend ostream& operator<<(ostream& out, Multiplication_table& multiplication_table)
+    {
+        for (vector <vector <int>>::iterator line = multiplication_table.table.begin(); line != multiplication_table.table.end(); line++)
+        {
+            for (vector<int>::iterator number = line->begin(); number != line->end(); number++)
+            {
+                out << *number << "\t";
+            }
+            out << "\n";
+        }
+        return out;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    Multiplication_table multiplication_table;
+    cout << multiplication_table;
+
+    return 0;
+}
